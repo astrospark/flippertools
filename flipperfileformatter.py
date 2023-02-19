190312fc2bb2787919323d08e11eef6e3be71523
Riley McArdle <riley@mcardle.org>
2023-02-19 15:31:45 -0600

    Fix dumping to file printing to stdout

--- flipperfileformatter.py
+++ flipperfileformatter.py
@@ -11,58 +11,61 @@ from flipperfile import FlipperFile
 class FlipperFileFormatter:
     @staticmethod
     def format(flipper_file: FlipperFile):
-        flipper_file._zip_file.filename = os.path.basename(flipper_file._zip_file.filename)
-        FlipperFileFormatter.print_zip_file(flipper_file._zip_file)
+        with io.StringIO() as output:
+            flipper_file._zip_file.filename = os.path.basename(flipper_file._zip_file.filename)
+            FlipperFileFormatter._format_zip_file(flipper_file._zip_file, output)
+            return output.getvalue()
 
     @staticmethod
-    def print_binary_file(file_bytes: bytes):
-        hexdump.hexdump(file_bytes)
-        print()
+    def _format_binary_file(file_bytes: bytes, output):
+        output.write(hexdump.hexdump(file_bytes, result='return'))
+        output.write('\n\n')
 
     @staticmethod
-    def print_json_file(file_bytes: bytes):
+    def _format_json_file(file_bytes: bytes, output):
         json_object = json.loads(file_bytes)
-        print(json.dumps(json_object, indent=2))
-        print()
+        output.write(json.dumps(json_object, indent=2))
+        output.write('\n\n')
 
     @staticmethod
-    def print_xml_file(file_bytes: bytes):
-        print(file_bytes.decode('utf8', 'strict'))
-        print()
+    def _format_xml_file(file_bytes: bytes, output):
+        output.write(file_bytes.decode('utf8', 'strict'))
+        output.write('\n\n')
 
     @staticmethod
-    def print_zip_file(zip_file: zipfile):
+    def _format_zip_file(zip_file: zipfile, output):
         namelist = zip_file.namelist()
         namelist.sort()
         for filename in namelist:
-            FlipperFileFormatter.print_file(zip_file, filename)
+            FlipperFileFormatter._format_file(zip_file, filename, output)
 
     @staticmethod
-    def print_file(zip_file: zipfile, filename: str):
+    def _format_file(zip_file: zipfile, filename: str, output):
         file_info = zip_file.getinfo(filename)
         full_path = f'{zip_file.filename}/{filename}'
         timestamp = '{0}-{1}-{2} {3}:{4:02}:{5:02}'.format(*file_info.date_time)
-        print(f'{full_path}\t{timestamp}\t({file_info.file_size} bytes)')
-        FlipperFileFormatter.print_rule()
+        output.write(f'{full_path}\t{timestamp}\t({file_info.file_size} bytes)\n')
+        FlipperFileFormatter._write_rule(output)
 
         file_bytes = zip_file.read(filename)
         extension = os.path.splitext(filename)[1]
         match extension:
             case '.json':
-                FlipperFileFormatter.print_json_file(file_bytes)
+                FlipperFileFormatter._format_json_file(file_bytes, output)
 
             case '.sb3':
                 scratch_io = io.BytesIO(file_bytes)
                 scratch_file = zipfile.ZipFile(scratch_io, 'r')
                 scratch_file.filename = full_path
-                FlipperFileFormatter.print_zip_file(scratch_file)
+                FlipperFileFormatter._format_zip_file(scratch_file, output)
 
             case '.svg':
-                FlipperFileFormatter.print_xml_file(file_bytes)
+                FlipperFileFormatter._format_xml_file(file_bytes, output)
 
             case _:
-                FlipperFileFormatter.print_binary_file(file_bytes)
+                FlipperFileFormatter._format_binary_file(file_bytes, output)
 
     @staticmethod
-    def print_rule(length=79):
-        print('-' * length)
+    def _write_rule(output, length=79):
+        output.write('-' * length)
+        output.write('\n')
