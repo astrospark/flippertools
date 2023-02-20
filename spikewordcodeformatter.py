3deea05765bf0983b06bfff480ffddab1813bb1a
Riley McArdle <riley@mcardle.org>
2023-02-19 18:05:06 -0600

    Improve support for My Blocks

--- spikewordcodeformatter.py
+++ spikewordcodeformatter.py
@@ -34,41 +34,46 @@ class SpikeWordCodeFormatter:
         if block['shadow']:
             return
 
-        tokens = [block['opcode']]
-
-        fields = block['fields']
-        for field in fields:
-            tokens.append(field)
-            tokens.append('=')
-            values = fields[field]
-            value = values[0]
-            tokens.append(value)
-
         inputs = block['inputs']
-        substack_block_id = None
-        for input in inputs:
-            if input == 'SUBSTACK':
+
+        if 'mutation' in block:
+            SpikeWordCodeFormatter._write_indent(output, indent)
+            output.write('run my block: ')
+            output.write(SpikeWordCodeFormatter._get_mutation(blocks, block['mutation'], inputs))
+            output.write('\n')
+        else:
+            tokens = [block['opcode']]
+
+            fields = block['fields']
+            for field in fields:
+                tokens.append(field)
+                tokens.append('=')
+                values = fields[field]
+                value = values[0]
+                tokens.append(value)
+
+            substack_block_id = None
+            for input in inputs:
+                if input == 'SUBSTACK':
+                    values = inputs[input]
+                    substack_block_id = values[1]
+                    break
+
+                tokens.append(input)
+                tokens.append('=')
                 values = inputs[input]
-                substack_block_id = values[1]
-                break
+                value = values[1]
+                if type(value) is list:
+                    tokens.append(value[1])
+                else:
+                    tokens.append('[' + SpikeWordCodeFormatter._get_value_block(blocks, value) + ']')
 
-            tokens.append(input)
-            tokens.append('=')
-            values = inputs[input]
-            value = values[1]
-            if type(value) is list:
-                tokens.append(value[1])
-            else:
-                tokens.append('[')
-                tokens.append(SpikeWordCodeFormatter._get_value_block(blocks, value))
-                tokens.append(']')
+            tokens.append('\n')
+            SpikeWordCodeFormatter._write_indent(output, indent)
+            output.write(' '.join(tokens))
 
-        tokens.append('\n')
-        SpikeWordCodeFormatter._write_indent(output, indent)
-        output.write(' '.join(tokens))
-
-        if substack_block_id:
-            SpikeWordCodeFormatter._format_stack(blocks, substack_block_id, output, indent + 1)
+            if substack_block_id:
+                SpikeWordCodeFormatter._format_stack(blocks, substack_block_id, output, indent + 1)
 
     @staticmethod
     def _get_value_block(blocks, block_id) -> str:
@@ -92,11 +97,38 @@ class SpikeWordCodeFormatter:
             if type(value) is list:
                 tokens.append(value[1])
             else:
-                tokens.append('[')
-                tokens.append(SpikeWordCodeFormatter._get_value_block(blocks, values[1]))
-                tokens.append(']')
+                tokens.append('[' + SpikeWordCodeFormatter._get_value_block(blocks, values[1]) + ']')
 
         return ' '.join(tokens)
+
+    @staticmethod
+    def _get_mutation(blocks, mutation, inputs):
+        tokens = []
+        for argument_id in SpikeWordCodeFormatter._parse_argument_ids(mutation['argumentids']):
+            values = inputs[argument_id]
+            value = values[1]
+            if type(value) is list:
+                tokens.append(value[1])
+            else:
+                tokens.append(SpikeWordCodeFormatter._get_value_block(blocks, values[1]))
+
+        mutation_string = mutation['proccode']
+        mutation_string = mutation_string.replace('%s', '[{}]')
+        mutation_string = mutation_string.format(*tokens)
+        return mutation_string
+
+    @staticmethod
+    def _parse_argument_ids(argument_ids: str) -> list:
+        # example: ["|dzTU:a[54zuspL$r67$","GeG.80K5h$OI96s[vrXa"]
+
+        argument_ids = argument_ids[1:-1]  # remove square brackets
+
+        argument_id_list = []
+        for argument_id in argument_ids.split(','):
+            argument_id = argument_id[1:-1]  # remove quotation marks
+            argument_id_list.append(argument_id)
+
+        return argument_id_list
 
     @staticmethod
     def _write_indent(output, indent: int = 0):
